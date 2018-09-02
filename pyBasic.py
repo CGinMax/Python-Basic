550bd4fd7101f5904e1e042c3b3eaf6dec4db103
CGinMax <35989023+CGinMax@users.noreply.github.com>
2018-09-02 22:35:21 +0800

    Start at Python

---
+++ pyBasic.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+
+coded = "MFC"
+notCode = "python"
+time = 10
+lutime = 13
+
+#if (coded == "MFC" and notCode == "python" and not (lutime < time or time > 12)) :
+#	print("I haven't study!")
+if (coded == "python"):
+	print("Oh!My God!")
+elif (coded == "C++STL"):
+	print("You are good!")
+elif (coded != notCode):
+	print("Don't repeat it")
+else:
+	print("You must fuck too much")
